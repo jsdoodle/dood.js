39b1fdef0a559665e2b6dd404c2a8d57bdc88839
jsdoodle admin <webdesign@jsdoodle.com>
2012-03-09 14:43:01 -0500

    Added dood.js

---
+++ dood.js
@@ -0,0 +1,45 @@
+(function (window, undefined) {
+		var dood = window.dood = new Function();,
+		
+		// Easier understanding of "extending" the dood prototype
+		dood.prototype.extend = function (name, fn) {
+			dood.prototype[name] = fn;
+		};
+		
+		// ==================================== //
+		//  BLUEPRINT - SIMPLE TEMPLATE ENGINE  //
+		// ==================================== //
+		dood.extend("blueprint", function(a, b, c) {
+			var proto = dood.blueprint.prototype;
+			if (a === "set" && b) {
+				proto.templates = proto.templates || {};
+				proto.templates[b] = (function() {
+					if (typeof c === "string") {
+						return String(c);
+					} else {
+						return c.innerHTML || c[0].innerHTML || c.toString() || null;
+					}
+				})();
+			} else {
+				if (proto.templates[a]) {
+					var temp = proto.templates[a];
+					if (jQuery) {
+						jQuery.each(b, function(d, e) {
+							temp = temp.replace("{{" + d + "}}", e);
+						});
+					} else {
+						for (var i in b) {
+							// ??
+							temp = temp.replace("{{" + b[i] + "}}", b[i][0]);
+						}
+					}
+					return temp;
+				}
+			}
+			
+			return "Error calling dood.blueprint";		
+		});
+		
+		
+		return;
+})();
